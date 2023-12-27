d7b6a1fc1ffce12febf58ab58295264509e5d66f
Mason <caohaison123@gmail.com>
2023-12-27 19:58:10 +1100

    Temporary view for habit details

--- GoodHabits/HabitDetailView.swift
+++ GoodHabits/HabitDetailView.swift
@@ -7,15 +7,64 @@
 
 import SwiftUI
 
+struct RectangleDivider: View {
+    var body: some View {
+        Rectangle()
+            .frame(height: 2)
+            .foregroundStyle(.gray)
+        //            .padding(.vertical)
+    }
+}
+
 struct HabitDetailView: View {
-    let habit: HabitItem
-    
+    var habit: HabitItem
     
     var body: some View {
-        ScrollView {
-            Text("Hello")
+        VStack {
+            VStack (alignment: .leading) {
+                Spacer()
+                Text(habit.name)
+                    .font(.largeTitle.bold())
+                    .foregroundStyle(.primary)
+                
+                Text(habit.description)
+                
+                Spacer()
+                RectangleDivider()
+                
+                Text("Increment count")
+                    .font(.title3.bold())
+                    .foregroundStyle(.primary)
+                
+            }
+  
+            VStack {
+                HStack {
+                    Button {
+                    } label: {
+                        Image(systemName: "minus.circle")
+                            .font(.system(size: 30))
+                    }
+                    
+                    Text(habit.displayCount)
+                        .frame(width: 70, height: 50)
+                        .background(.thickMaterial)
+                        .clipShape(.rect(cornerRadius: 10))
+                        .padding(.horizontal)
+                    
+                    Button {
+                    } label: {
+                        Image(systemName: "plus.circle")
+                    }
+                    .font(.system(size: 30))
+                }
+                
+                Spacer()
+            }
+            
         }
         .navigationTitle(habit.name)
+        .padding(.horizontal)
     }
 }
 
