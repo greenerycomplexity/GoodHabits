8899e54639989a414654af5d62d0023b5e243157
Mason <caohaison123@gmail.com>
2023-12-27 18:00:18 +1100

    Initial Commit

---
+++ GoodHabits/GoodHabitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoodHabitsApp.swift
+//  GoodHabits
+//
+//  Created by Son Cao on 27/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct GoodHabitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
