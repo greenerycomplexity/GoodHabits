62fe57f8f18055c3e0f4842073651941b7e09e3b
Mason <caohaison123@gmail.com>
2023-12-30 13:20:31 +1100

    Finished UI Overhaul. Added habit deletion mechanism.

--- GoodHabits/HabitDetailView.swift
+++ GoodHabits/HabitDetailView.swift
@@ -7,103 +7,135 @@
 
 import SwiftUI
 
-struct RectangleDivider: View {
+struct HabitDetailView: View {
+    let emptyCompletionsTitle = "None so far, get into the habit today!"
+    @Binding var navPath: [HabitItem]
+    @State var habit: HabitItem
+    var habits: Habits
+    
+    
+    @State private var showDeleteWarning = false
+    let deleteWarningTitle = "Delete this Habit?"
+    let deleteWarningMessage = "All of this habit's completions data will be deleted"
+    
     var body: some View {
-        Rectangle()
-            .frame(height: 2)
-            .foregroundStyle(.gray)
+        ZStack {
+            Color.gray.opacity(0.2)
+                .ignoresSafeArea()
+            VStack {
+                //            Habit Circle
+                ZStack {
+                    Circle()
+                        .containerRelativeFrame(.horizontal) { width, axis in
+                            width * 0.7
+                        }
+                        .foregroundStyle(.white)
+                        .overlay(
+                            Circle()
+                                .strokeBorder(.green, lineWidth: 6)
+                        )
+                    
+                    VStack {
+                        Text(habit.icon)
+                            .font(.system(size: 50))
+                        
+                        Text(habit.name)
+                            .font(.title.bold())
+                            .foregroundStyle(.black)
+                            .fontDesign(.rounded)
+                        
+                        Button {
+                            //                            Find the index of the current habit in the Observable class' array
+                            if let index = habits.items.firstIndex(of: habit) {
+                                //                                Add new completion
+                                withAnimation {
+                                    habit.completions.insert(Date.now, at: 0)
+                                    //                                Add the new struct to the array where the old HabitItem was, so Swift knows it changed (for main Home view)
+                                    habits.items[index] = habit
+                                }
+                            }
+                            
+                            
+                        } label: {
+                            Text("Track habit")
+                                .fontDesign(.rounded)
+                        }
+                        .buttonStyle(.borderedProminent)
+                        .buttonBorderShape(.capsule)
+                        .padding(.top)
+                    }
+                }
+                .padding(.bottom)
+                
+                
+                //                List of previous completions
+                HStack {
+                    VStack (alignment: .leading) {
+                        Text("Previous completions")
+                            .font(.title2.bold())
+                            .fontDesign(.rounded)
+                        
+                        CompletionListView(habit: habit)
+                    }
+                    Spacer()
+                }
+                .padding(.horizontal)
+                
+                Spacer()
+                
+            }
+        }
+        .preferredColorScheme(.dark)
+        .navigationTitle("Habit")
+        .navigationBarTitleDisplayMode(.inline)
+        .toolbar {
+            ToolbarItem(placement: .destructiveAction) {
+                Button("Delete") {
+                    showDeleteWarning = true
+                }
+            }
+        }
+        .alert(deleteWarningTitle, isPresented: $showDeleteWarning) {
+            Button("Okay", role: .destructive) {
+                if let index = habits.items.firstIndex(of: habit) {
+                    habits.items.remove(at: index)
+                    navPath.removeAll()
+                }
+            }
+            Button("Cancel", role: .cancel) {}
+        } message: {
+            Text(deleteWarningMessage)
+        }
     }
 }
 
-struct HabitDetailView: View {
-    @Binding var navPath: [HabitItem]
-    let emptyCompletionsTitle = "None so far, get into the habit today!"
-    
+struct CompletionListView: View {
     var habit: HabitItem
-    var habits: Habits
     
     var body: some View {
-        VStack {
-            VStack (alignment: .leading) {
-                Spacer()
-                Text(habit.name)
-                    .font(.largeTitle.bold())
-                    .foregroundStyle(.primary)
-                
-                Text(habit.description)
-                
-                Button("Delete activity", role: .destructive) {
-                    if let index = habits.items.firstIndex(of: habit) {
-                        habits.items.remove(at: index)
-                        navPath.removeAll()
-                    }
-                }
-                .buttonStyle(.borderedProminent)
-                
-                Text("Previous completions: \(habit.displayCount)")
-                Spacer()
-                RectangleDivider()
-                
-            }
-            
-            VStack {
+        //                Scroll view of all habit completions
+        ScrollView {
+            ForEach(habit.completions, id:\.self) { completion in
                 HStack {
-                    Text("Increment count")
-                        .font(.title3.bold())
-                        .foregroundStyle(.primary)
-                    
+                    VStack (alignment: .leading) {
+                        Text(completion.formatted(date: .abbreviated, time: .omitted))
+                            .font(.headline)
+                            .foregroundStyle(.white)
+                        
+                        Text(completion.formatted(date: .omitted, time: .shortened))
+                            .font(.subheadline)
+                            .foregroundStyle(.gray)
+                            .lineLimit(1)
+                    }
+                    .padding()
                     Spacer()
                     
-                    //                    Create temp habit to save replace current habit in the habits array
-                    //                    since HabitItem is just a struct and Swift won't know it changed.
-                    Button {
-                        var tempHabit = habit
-                        //                        Add new completion into Date array
-                        let currentDate = Date.now
-                        withAnimation {
-                            tempHabit.completions.insert(currentDate, at: 0)
-                        }
-                        
-                        if let index = habits.items.firstIndex(of: habit) {
-                            habits.items[index] = tempHabit
-                        }
-                    } label: {
-                        Image(systemName: "plus.circle")
-                            .font(.system(size: 30))
-                    }
                 }
-                
-                if habit.completions.count != 0 {
-                    List {
-                        ForEach(habit.completions, id:\.self) { completion in
-                            VStack (alignment: .leading) {
-                                Text(completion.formatted(date: .abbreviated, time: .omitted))
-                                    .font(.headline)
-                                
-                                Text(completion.formatted(date: .omitted, time: .shortened))
-                            }
-                            .listRowSeparator(.hidden)
-                            .padding()
-                            .background(.thinMaterial)
-                            .clipShape(.rect(cornerRadius: 15))
-                            
-                            
-                        }
-                    }
-                    .listStyle(.plain)
-                    .background(.blue)
-                } else {
-                    Text(emptyCompletionsTitle)
-                }
-                
-                
-                
-                
-                Spacer()
+                .frame(maxWidth:.infinity)
+                .background(.white.opacity(0.1))
+                .clipShape(.rect(cornerRadius: 15))
             }
-            
         }
-        .padding(.horizontal)
     }
 }
 
